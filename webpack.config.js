40db79f8819290b3e3176ec7a0f0b68706e3e536
Fahd Rahali <rahali.fahd@gmail.com>
2022-03-22 15:42:35 +0100

    set up html webpack plugin

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+};
