49f5b7a1e13d4174edf556d90d99788b07f87acc
Fahd Rahali <rahali.fahd@gmail.com>
2022-03-23 23:32:20 +0100

    remove all hardcoded items from the tasks array

--- modules/tasks.js
+++ modules/tasks.js
@@ -1,24 +1,4 @@
 const tasks = [
-  {
-    index: 1,
-    description: 'complete part1 todo list project',
-    completed: false,
-  },
-  {
-    index: 2,
-    description: 'complete part2 todo list project',
-    completed: false,
-  },
-  {
-    index: 3,
-    description: 'share my project ToDolist on twitter',
-    completed: true,
-  },
-  {
-    index: 4,
-    description: 'add two function add and remove',
-    completed: false,
-  },
 ];
 
 export default tasks;
