59dffb24f7cf5df8386c83e08fe121cd84c2a8bb
Fahd Rahali <rahali.fahd@gmail.com>
2022-03-23 16:46:10 +0100

    fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,31 +1,15 @@
 import './style.css';
 import tasks from '../modules/tasks.js';
 
-const getTaskItemList = () => {
-  let taskItemList = '';
+const render = () => {
+  const taskListContainer = document.querySelector('.todolist');
   tasks.map((task) => {
-    taskItemList += `<li>
+    taskListContainer.innerHTML += `<li>
           <input type="checkbox" id=${task.index}>
           <label for=${task.index}>${task.description}</label>
       </li>`;
     return task;
   });
-  return taskItemList;
-};
-
-const render = () => {
-  const taskListContainer = document.createElement('ul');
-  taskListContainer.className = 'todolist';
-  document.body.appendChild(taskListContainer);
-
-  taskListContainer.innerHTML = `<li>Today's To Do</li>
-  <li>
-  <input type='text' name='task' id='task' placeholder='Add to your list...'>
-  </li>
-  `;
-
-  taskListContainer.innerHTML += getTaskItemList();
-
   taskListContainer.innerHTML += `<li class="clear">
   <a>Clear all completed</a>
   </li>`;
